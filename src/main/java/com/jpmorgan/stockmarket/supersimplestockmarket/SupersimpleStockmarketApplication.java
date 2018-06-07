3b9f096668f90c27f129161fef07bea500570d14
jsnrao <jsn0409@gmail.com>
2018-06-07 16:07:41 +0100

    first commit

---
+++ src/main/java/com/jpmorgan/stockmarket/supersimplestockmarket/SupersimpleStockmarketApplication.java
@@ -0,0 +1,12 @@
+package com.jpmorgan.stockmarket.supersimplestockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupersimpleStockmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupersimpleStockmarketApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jpmorgan/stockmarket/supersimplestockmarket/SupersimpleStockmarketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jpmorgan.stockmarket.supersimplestockmarket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupersimpleStockmarketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
